6054f8a72d8df3095448231388e306feb875b35a
unknown <asef18766@gmail.com>
2023-09-12 22:48:52 +0800

    add basic dropper

--- Assets/Scripts/UI/DraggableItem.cs
+++ Assets/Scripts/UI/DraggableItem.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
 namespace UI
 {
@@ -9,10 +10,16 @@ namespace UI
         [SerializeField] private RectTransform rect;
         private RectTransform _mRect;
         private Rigidbody2D _rigidbody2D;
+        public bool droppable;
+        private float _oriGravScale;
+        
         private void Start()
         {
             _mRect = GetComponent<RectTransform>();
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            _oriGravScale = _rigidbody2D.gravityScale;
+
+            _rigidbody2D.gravityScale = 0;
         }
 
         private Vector2 GetMousePos()
@@ -24,18 +31,31 @@ namespace UI
         public void OnBeginDrag(PointerEventData eventData)
         {
             _mRect.anchoredPosition = GetMousePos();
-            _rigidbody2D.simulated = false;
+            _rigidbody2D.gravityScale = 0;
         }
 
         public void OnDrag(PointerEventData eventData)
         {
+            if (!droppable) return;
             _mRect.anchoredPosition = GetMousePos();
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
-            Debug.Log("end drag");
-            _rigidbody2D.simulated = true;
+            if (droppable)
+                _rigidbody2D.gravityScale = _oriGravScale;
+            
+        }
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (!col.CompareTag("DropBound")) return;
+            droppable = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D col)
+        {
+            if (!col.CompareTag("DropBound")) return;
+            droppable = false;
         }
     }
 }
